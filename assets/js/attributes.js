1cc7e95a6086a6e3277813497f51821a8d3db299
Kevin Kim <kevintaekim80@gmail.com>
2015-04-07 17:37:06 -0700

    add to attributes.js for style steps module

--- assets/js/attributes.js
+++ assets/js/attributes.js
@@ -67,4 +67,200 @@ $(document).ready(function() {
 
 
 
+$(document).ready(function() {
+
+
+        $('body').popover({selector: '[data-toggle=popover]'});
+
+        var stateMap = {
+            playground: document.getElementById('playground'),
+            idButton : document.getElementById('idButton'),
+            queryButton : document.getElementById('querySelectorButton'),
+            classButton: document.getElementById('classButton'),
+            tagButton: document.getElementById('tagButton'),
+            queryAllButton: document.getElementById('queryAllButton')
+        };
+
+
+
+        //START IF getBYIdModule//
+        var getByIdModule = (function() {
+
+            var selections;
+
+            var inputSelection = function() {
+                var inputVal = document.getElementsByClassName('idInput')[0];
+                return inputVal.value;
+            }, 
+
+            clearPrevSel = function() {
+
+                var prevSelected = $(stateMap.playground).find('.selected');
+
+                for (var i =0; i<prevSelected.length; i++) {
+                    $(prevSelected[i]).removeClass('selected');
+                }
+
+            },
+            
+            //function to bind to trigger//
+            idElSelect = function() {
+                var idInput = "#"+inputSelection();
+                selections = $(stateMap.playground).find(idInput);
+
+               //clear previous selections//
+                clearPrevSel();
+
+                //add the 'selected class to highlight background'
+               selections.addClass('selected');
+
+            },
+
+
+
+
+            //init//
+
+            init = function() {
+                var button = stateMap.idButton;
+                button.addEventListener('click', function() {
+                  idElSelect();
+                    if (selections.length === 0) {
+                        alert('No HTML Elements were selected!  Try again');
+                    }
+                }, false);
+            };
+
+
+            return {
+                init: init
+            };
+
+
+        }());
+
+ //END oF getBYIdModule//
+
+
+
+
+var stepModule = (function() {
+
+    var step1But,
+        step2But,
+        step3But;
+
+    
+
+
+    function step1() {
+        var stepCodeTxt = $('.stepCodeTxt');
+        var selected = $('.selected');
+        var selectedId = selected.attr('id');
+
+        var newStepCodeTxt = 'document.getElementById(\''+ selectedId + '\')';
+
+        stepCodeTxt.html(newStepCodeTxt);
+
+    }
+
+
+
+    function step2() {
+        var stepCodeTxt = $('.stepCodeTxt');
+        var selected = $('.selected');
+        var newRating = $('#rangeWarning').text() + '%';
+        var newBgColor = $('.backgroundInput').val();
+
+        function setNewTxt() {
+            var currentTxt = stepCodeTxt.html();
+
+            var newTxt = currentTxt + '.style.background = ' + newBgColor + ';' + '</br>';
+                newTxt += currentTxt + '.style.width = ' + newRating + ';';
+
+            stepCodeTxt.html(newTxt);
+
+        }
+
+        setNewTxt();
+
+    }
+
+
+
+    function step3() {
+        var selected = $('.selected');
+        var newRating = $('#rangeWarning').text() + '%';
+        var newBgColor = $('.backgroundInput').val();
+
+
+        selected.css({
+            width: newRating,
+            background: newBgColor
+        });
+        selected.removeClass('selected');
+        selected.text(newRating);
+
+    }
+
+
+
+
+
+    step1But = $('#step1But');
+    step2But = $('#step2But');
+    step3But = $('#step3But');
+
+    step1But.on('click', step1);
+    step2But.on('click', step2);
+    step3But.on('click', step3);
+
+}());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//init all modules//
+
+getByIdModule.init();
+
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
